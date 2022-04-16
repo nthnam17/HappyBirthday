4c8fc1d39895022f032b06233dc8c4f7f7e01e80
ngthehoainam <nthnam.dev@gmail.com>
2022-04-16 10:43:55 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Nhung';
+const giftUrl = 'https://www.facebook.com/nam.nguyenthehoai.3';
+const eventName = 'Happy Birthday';
+const titleCard = 'Gửi ngàn lời chúc';
+const contentCard = 'Chúc Nhung có một ngày sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với Nhung trong hôm nay và cả những ngày sau.';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
