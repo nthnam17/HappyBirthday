41dc9be0855a6e3a1ee20f67566ccdbf95d7e7ca
nthnam17 <76147564+nthnam17@users.noreply.github.com>
2022-09-13 02:34:27 +0700

    Update config.js
    thay doi kieu du lieu

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ const eventName = 'Happy Birthday';
 const titleCard = 'Gửi ngàn lời chúc';
 const contentCard = 'Chúc Nhung có một ngày sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với Nhung trong hôm nay và cả những ngày sau.';
 
-const giftImage = 'hot-girl.png';
+const giftImage = 'hot-girl.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
