e5cf8183a4eb5e4ec86ac5c515aa1cf983cf2896
nthnam17 <76147564+nthnam17@users.noreply.github.com>
2022-09-13 03:34:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ const eventName = 'Happy Birthday';
 const titleCard = 'Gửi ngàn lời chúc';
 const contentCard = 'Chúc mừng sinh nhật em. mong mọi điều tốt đẹp nhất sẽ đến với em.';
 
-const giftImage = 'hot-girl.jpg';
+const giftImage = 'hot-girl.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
